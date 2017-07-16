8a6983fdfa45c1442feb54e47dd2f7f27b104edb
Darla Torres <dmt1@williams.edu>
2017-07-16 00:40:14 +0000

    adding .py

---
+++ Function and Modules.py
@@ -0,0 +1,7 @@
+def userInt(prompt):
+    print prompt
+    num = int(raw_input())
+    return num
+
+kmWeekday= userInt("How many km did you travel on a weekday?")
+kmWeekend= userInt("How many km did you travel on a weekday?")
